d17fbd8a9bd9f635d508f8cd2e1090dfd2a8f36b
Leonelbss <156707596+Leonelbss@users.noreply.github.com>
2026-05-23 21:36:37 -0100

    Add files via upload

---
+++ biblioteca.h
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LIVROS 100
+typedef struct {
+    char titulo[50];
+    char nomeAutor [50];
+    int anoPublicado;
+    int quantidadeDisponivel;   
+    int codigoLivro;
+
+
+
+}Livro;
+
