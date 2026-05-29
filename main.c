189cd6c42aae9cffcff5a5643dc5fb2ee8d283b7
Jota935 <jarnitimas@gmail.com>
2026-05-29 22:20:24 -0100

    Correcao dos erros no main.c e biblioteca.c

--- main.c
+++ main.c
@@ -1,46 +1,53 @@
-#include <string.h>
 #include <stdio.h>
 #include "biblioteca.h"
 
+void mostrarMenu() {
+    printf("\n===== BIBLIOTECA =====\n");
+    printf("1. Adicionar Livro\n");
+    printf("2. Listar Livros\n");
+    printf("3. Procurar Livro\n");
+    printf("4. Alugar Livro\n");
+    printf("5. Atualizar Quantidade\n");
+    printf("6. Apagar Livro\n");
+    printf("7. Total de Livros\n");
+    printf("8. Sair\n");
+    printf("Opcao: ");
+}
 
-int main(){
+int main() {
     int opcao = 0;
-    while (opcao !=  8)
-    {
+
+    while(opcao != 8) {
+
         mostrarMenu();
         scanf("%d", &opcao);
-        switch(opcao)
-{
-    case 1://Adicionar um novo livro
-        printf("opcao 1 selecionada");
-        break;
-    case 2://Listar todos os livros cadastrados.
-        printf("opcao 2 selecionada");
-        break; 
-    case 3:
-        printf("opcao 3 selecionada");//Procurar um livro pelo código.
-        break;
-    case 4://Procurar um livro pelo código.
-        printf("opcao 4 seleconada");
-        break;           
-    case 5://Atualizar a quantidade de exemplares de um livro.
-        printf("opcao 5 selecionda");
-        break; 
-    case 6://Atualizar a quantidade de exemplares de um livro.
-        printf("opcao 6 selecionada");
-        break;   
-    case 7://Mostrar o total de livros registados.
-        printf("opcao 7 seleciondada");
-        break;        
 
-    case 8://Sair do programa.
-        printf("opcao 8 selecionada");
-        break;
+        switch(opcao) {
 
-    default:
-        printf("opcao invalida");
-}
-        /*  code */
+            case 1:
+                adicionarLivro();
+                break;
+
+            case 2:
+                listarLivros();
+                break;
+
+            case 4:
+                alugarLivro();
+                break;
+
+            case 6:
+                apagarLivro();
+                break;
+
+            case 8:
+                printf("Programa encerrado.\n");
+                break;
+
+            default:
+                printf("Opcao invalida.\n");
+        }
     }
-    
+
+    return 0;
 }
